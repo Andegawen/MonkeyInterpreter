a1e15f10129dbdb10afc184122d2d65465fa30cc
krzysztof.jan.kroczak@gmail.com <krzysztof.jan.kroczak@gmail.com>
2018-03-02 15:30:22 +0000

    Add return statement

---
+++ MonkeyInterpreter/Parsers/ReturnStatement.cs
@@ -0,0 +1,20 @@
+namespace MonkeyInterpreter.Parsers
+{
+    public class ReturnStatement : IStatement
+    {
+        public ReturnStatement(Token statementToken, IExpression value)
+        {
+            StatementToken = statementToken;
+            Value = value;
+        }
+
+        public Token StatementToken {get;private set;}
+        public Identifier Identifier {get;private set;}
+        public IExpression Value {get; private set;}
+
+        public override string ToString()
+        {
+            return $"Statement {StatementToken.Type} with Identifier `{Identifier.Name}` {Value}";
+        }
+    }
+}
